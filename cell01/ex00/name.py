df077ae5011d055f9ea82dd0e4c1225249ca7c97
Suwijak Promsatid <suwijak.pak@gmail.com>
2026-02-11 20:09:08 +0700

    cell01-02

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name="Suwijak"
+last_name="Promsatid"
+print(f"{first_name} {last_name}")
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name="Suwijak"
+last_name="Promsatid"
+whole_name=first_name+" "+last_name
+print(whole_name)
